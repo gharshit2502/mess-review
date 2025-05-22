fa74889e0583275c50409e1e1e2f16bc2973a67f
gharshit2502 <gharshi2502@gmail.com>
2025-05-23 00:32:40 +0530

    after adding css and js

---
+++ script.js
@@ -0,0 +1,20 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwQJgf1uJLuM3tECzvpwgABVVxjZNHUxzComnkQptAvnRBRa0NOsrvEvHNUfB8xtuG0ew/exec';
+
+document.getElementById('reviewForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const form = e.target;
+  const formData = new FormData(form);
+
+  fetch(scriptURL, {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    document.getElementById('status').textContent = '✅ Review Submitted!';
+    form.reset();
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    document.getElementById('status').textContent = '⚠️ Submission failed.';
+  });
+});
